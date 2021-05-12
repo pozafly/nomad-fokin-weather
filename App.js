63488a16d00386783ef9b474a5e60b6b56c85444
HwangSunTae <pozafly@gmail.com>
2021-05-12 20:42:59 +0900

    weather API KEY insert

--- App.js
+++ App.js
@@ -1,6 +1,31 @@
-import React from 'react';
-import Loading from './Loading'
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+import Loading from './Loading';
+import * as Location from 'expo-location';
 
 export default function App() {
-  return <Loading />;
+  const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getLocation = async () => {
+    try {
+      await Location.getForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      
+      // Send to API and get weather!
+      console.log(latitude, longitude)
+      setIsLoading(false);
+
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return isLoading ? <Loading /> : null;
 }
